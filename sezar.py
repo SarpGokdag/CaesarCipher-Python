21bf0269dd4ac8c2b2bffa886355f06f15bd5747
Sarp Gökdağ <sarpgo@icloud.com>
2017-04-23 11:41:22 +0300

    Update sezar.py

--- sezar.py
+++ sezar.py
@@ -2,11 +2,11 @@
 ##Youtube Video Link --> 
 plaintext = input('Çevrilecek Metin : ')
 alfabe = "abcçdefghıijklmnoöprsştuüvyz"
-yanakaydırma = 3
+anahtar = 3
 kripto = ''
 
 for c in plaintext :
     if c in alfabe:
-        kripto += alfabe[(alfabe.index(c)-yanakaydırma)%(len(alfabe))]
+        kripto += alfabe[(alfabe.index(c)-anahtar)%(len(alfabe))]
 
 print('İşte Kriptolu Mesajınız :' + kripto)
